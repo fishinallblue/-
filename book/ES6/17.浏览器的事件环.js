09ed4ed7c2bbe2d17aafd1b3e1119c8ba923e66d
junhe213310 <junhe213310@sohu-inc.com>
2019-01-29 18:29:37 +0800

    info change

--- book/ES6/17.浏览器的事件环.js
+++ book/ES6/17.浏览器的事件环.js
@@ -1,1 +1,5 @@
 // 浏览器中的事件环，会先执行主站中的代码执行后会清空微任务队列 -> 取一个宏任务执行 -> 再去清空微任务 -> 再取一个宏任务
+
+// 宏任务有哪些 微任务有哪些
+// microtask 微任务 Promise MutaiionOberserve MessageChannel
+// macrotask 宏任务 setImmediate(IE 下面才有) setTimeOut
