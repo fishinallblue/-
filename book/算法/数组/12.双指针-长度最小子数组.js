f41363bfc760afee16641abe01ae442f55f77a88
hediahe <hediahe@tencent.com>
2022-11-24 13:58:49 +0800

    feat: 滑动窗口提交

--- book/算法/数组/12.双指针-长度最小子数组.js
+++ book/算法/数组/12.双指针-长度最小子数组.js
@@ -1,9 +1,45 @@
 // 题目描述：https://leetcode.cn/problems/minimum-size-subarray-sum/
-
 /**
+ * @param {number} target
  * @param {number[]} nums
- * @return {number[][]}
+ * @return {number}
  */
 var minSubArrayLen = function(target, nums) {
-
+  let count = 0;
+  if (nums?.length) {
+    let i = 0, k = 1;
+    let plus = nums[0];
+    while (i < nums.length) {
+      if (nums[i] === target) {
+        return 1;
+      }
+      if (k < nums.length) {
+        while(plus < target && k < nums.length) {
+          plus = plus + nums[k]
+          k++
+          if (plus === target) {
+            if (count === 0 || count > k - i) {
+              count = k - i;
+            }
+            plus = plus - nums[i];
+            i = i + 1;
+          }
+        }
+        while(plus > target && i < k) {
+          plus = plus - nums[i]
+          i++
+          if (plus === target) {
+            if (count === 0 || count > k - i) {
+              count = k - i;
+            }
+            plus = plus - nums[i];
+            i = i + 1;
+          }
+        }
+      }
+    }
+  }
+  console.log('count', count)
+  return count;
 };
+minSubArrayLen(7, [2,3,1,2,4,3])
